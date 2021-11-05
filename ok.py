e32752476e538da46c4bf8c22bbd4483f9d7b6c1
amandabekturganova <93776138+amandabekturganova@users.noreply.github.com>
2021-11-05 19:44:00 +0600

    Create ok.py

---
+++ ok.py
@@ -0,0 +1,20 @@
+# a =(1,2,3,4,5)
+
+# a = (15,95,100)
+# print(a[:2])
+
+# a =(99, 79, 97, 'wear', 'repit')
+# print(a)
+
+# a =['Nick', 'Ann', 'Pit']
+# s =' '
+# print(s.join(a))
+
+# a =[1,2,3,4,5,6,7,8,9]
+# b =[10, 11, 12, 13, 14, 15]
+# a.append(b)
+# print(a)
+
+# names =['Jack', 'Jimmy', 'Jackson', 'Jhon', 'Jackson', 'Jhon', 'Jimmy', 'Jackson']
+
+# while True: print(eval(input('Введите число')))
